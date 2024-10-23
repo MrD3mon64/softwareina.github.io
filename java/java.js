69584fd74ae28f5336c20f6803a0abf986e5ae6d
MrD3mon64 <angelmanciajovel@gmail.com>
2024-10-23 12:58:55 -0600

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,24 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="english/indexen.html";
+    }else{
+        location.href="/index.html"
+    }
+}
+
+
+
+
+let slideIndex = 0;
+
+function moveSlide(n) {
+    const slides = document.querySelectorAll('.carousel-item');
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex + n + slides.length) % slides.length;
+    slides[slideIndex].classList.add('active');
+}
+
